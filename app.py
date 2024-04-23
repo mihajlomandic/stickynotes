6eb79bed9eff090b833350c39ac0ab8811f659d1
Mihajlo Mandić <86262609+mihajlomandic@users.noreply.github.com>
2024-04-23 18:05:42 +0200

    Added functionality for adding and deleting tasks

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
 from flask import Flask, render_template, url_for, request, redirect
 import sqlite3
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder='templates', static_url_path='', static_folder='static')
 
 
 def create_database():
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect('todo.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task TEXT)''')
     conn.commit()
     conn.close()
 
 
-@app.route("/")
+@app.route('/')
 def index():
     create_database()
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect('todo.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM tasks")
+    c.execute('SELECT * FROM tasks')
     tasks = c.fetchall()
     conn.close()
     return render_template("index.html", tasks=tasks)
 
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO tasks (task) VALUES (?)', (task,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
     create_database()
     app.run(debug=True)
